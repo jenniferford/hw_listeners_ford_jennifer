cdf8886c744a814e24c7e41f3c7c46f8d927f53a
[Jennifer Ford] <[jennifer.ford.design@gmail.com]>
2015-11-06 23:21:29 -0500

    added first 2 event listeners

---
+++ js/main.js
@@ -0,0 +1,10 @@
+var headerColor = document.getElementById( 'header' );
+        headerColor.addEventListener('click', function(){
+        headerColor.style.backgroundColor = '#FFB7B7';
+    });
+
+
+var backgroundColor = document.getElementById( 'container' );
+        backgroundColor.addEventListener('mouseover', function(){
+        backgroundColor.style.backgroundColor = '#cdcdb4';
+    });
