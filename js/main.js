58aa1158f96956e230acd14d9916c599631e3be4
[Jennifer Ford] <[jennifer.ford.design@gmail.com]>
2015-11-06 23:42:04 -0500

    added third custom event listener

--- js/main.js
+++ js/main.js
@@ -8,3 +8,8 @@ var backgroundColor = document.getElementById( 'container' );
         backgroundColor.addEventListener('mouseover', function(){
         backgroundColor.style.backgroundColor = '#cdcdb4';
     });
+
+        var image = document.getElementById("image_click");
+image.addEventListener("click", function(){
+    this.src="http://www.quickmeme.com/img/64/64551a2f6b1248181955f8e5c16e8c9041401ab57875d7c5fd0634e3742fddd8.jpg";
+});
